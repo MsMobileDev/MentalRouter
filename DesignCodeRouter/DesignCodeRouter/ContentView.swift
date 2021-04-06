852e26e63b77fb7955292e972489f1514a43cb46
Olga Shilenko <oshilenko@mentalstack.com>
2021-04-06 19:59:25 +0300

    Initial commit

---
+++ DesignCodeRouter/DesignCodeRouter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeRouter
+//
+//  Created by Olga Shilenko on 06.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
